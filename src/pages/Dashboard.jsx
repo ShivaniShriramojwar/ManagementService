ff77a15bdefbeec1698d0e2687f034fe46950bfd
Shivani <shivani.bhaskar@nxtgen.com>
2025-06-13 10:29:56 +0530

    dashboard changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,9 @@
-import { serverSvg } from "@/assets/common";
+import {
+  checkMarkSvg,
+  lightBolt,
+  redIncident,
+  serverSvg,
+} from "@/assets/common";
 import {
   Card,
   CardAction,
@@ -16,7 +21,7 @@ const Dashboard = () => {
         <Card>
           <CardHeader>
             <CardTitle className="flex justify-between">
-              <h1 className="text-2xl">Status Dashboard</h1>
+              <h1 className="text-2xl font-semibold">Status Dashboard</h1>
 
               <div className="flex items-center gap-3">
                 <span className="text-gray-800 font-medium">
@@ -59,46 +64,72 @@ const Dashboard = () => {
         <div className="w-1/4">
           <Card>
             <CardHeader>
-              <CardTitle>Card Title</CardTitle>
-              <CardDescription>Card Description</CardDescription>
-              <CardAction>Card Action</CardAction>
+              <CardTitle>
+                {" "}
+                <div className="flex justify-between">
+                  {" "}
+                  <h6 className="text-gray-500">Services Online</h6>
+                  <img src={checkMarkSvg} />
+                </div>
+              </CardTitle>
             </CardHeader>
             <CardContent>
-              <p>Card Content</p>
+              <div className="flex flex-col gap-2 ">
+                <div className="font-bold text-3xl p-2 bg-[#b6ebd1] text-[#12AE60]">
+                  6
+                </div>
+                <div className="font-bold text-xs p-2 bg-[#b6ebd1] text-[#12AE60] border rounded-2xl">
+                  75% uptime
+                </div>
+              </div>
             </CardContent>
-            <CardFooter>
-              <p>Card Footer</p>
-            </CardFooter>
+          </Card>
+        </div>
+
+        <div className="w-1/4">
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                {" "}
+                <div className="flex justify-between">
+                  {" "}
+                  <h6 className="text-gray-500">Active Incidents</h6>
+                  <img src={redIncident} />
+                </div>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-col gap-2 ">
+                <div className="font-bold text-3xl p-2 bg-[#fee2e2] text-[#dc2626]">
+                  2
+                </div>
+                <div className="font-bold text-xs p-2 bg-[#fee2e2] text-[#dc2626] border rounded-2xl">
+                  +1 today
+                </div>
+              </div>
+            </CardContent>
           </Card>
         </div>
         <div className="w-1/4">
           <Card>
             <CardHeader>
-              <CardTitle>Card Title</CardTitle>
-              <CardDescription>Card Description</CardDescription>
-              <CardAction>Card Action</CardAction>
+              <CardTitle>
+                {" "}
+                <div className="flex justify-between">
+                  {" "}
+                  <h6 className="text-gray-500">Avg Response Time</h6>
+                  <img src={lightBolt} />
+                </div>
+              </CardTitle>
             </CardHeader>
             <CardContent>
-              <p>Card Content</p>
+              <div className="flex flex-col gap-2 ">
+                <div className="font-bold text-3xl p-2">1.2s</div>
+                <div className="font-bold text-xs p-2 bg-[#b6ebd1] text-[#12AE60] border rounded-2xl">
+                  -0.3s improved
+                </div>
+              </div>
             </CardContent>
-            <CardFooter>
-              <p>Card Footer</p>
-            </CardFooter>
-          </Card>
-        </div>
-        <div className="w-1/4">
-          <Card>
-            <CardHeader>
-              <CardTitle>Card Title</CardTitle>
-              <CardDescription>Card Description</CardDescription>
-              <CardAction>Card Action</CardAction>
-            </CardHeader>
-            <CardContent>
-              <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-              <p>Card Footer</p>
-            </CardFooter>
           </Card>
         </div>
       </div>
